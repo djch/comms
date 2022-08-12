3b83999f99a32acd43e3588f07bf6bdc5d849a1a
Dan Charlesworth <dcharlesworth@me.com>
2022-08-12 13:29:01 +1000

    Some basic layouts and styles

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,8 +10,21 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        // sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      boxShadow: {
+        'soft': '-0.2rem 0.2rem 1rem rgba(0, 0, 0, 0.1)',
+        'softer': '-0.4rem 0.4rem 2rem rgba(0, 0, 0, 0.1)',
+      },
+      keyframes: {
+        dissolve: {
+          '0%, 100%': { opacity: '0' },
+          '5%, 60%': { opacity: '1' },
+        }
+      },
+      animation: {
+        dissolve: 'dissolve 7s 300ms both',
+      }
     },
   },
   plugins: [
